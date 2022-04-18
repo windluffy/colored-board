70f77e8ed0c305dc7ce43ed15334d3587e187ea3
windluffy <windnarsas@gmail.com>
2022-04-19 01:52:32 +0700

    Display colored board

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Board from "./components/Board";
+import {generate} from "./utils/matrix.util";
+import randomColor from "randomcolor";
 
 function App() {
+  const [matrixTiles, setMatrixTile] = useState(
+    () => generate([8, 8], () => randomColor())
+  )
+
   return (
     <div className="App">
       <div style={{margin: "auto"}}>
-        <Board initialMatrixTiles={[
-          ['red', 'red'],
-          ['yellow', 'cyan']
-        ]} />
+        <Board initialMatrixTiles={matrixTiles} />
       </div>
     </div>
   );
